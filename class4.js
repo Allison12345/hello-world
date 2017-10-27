b039fb871599b31464bf989f0d589971558e7ae5
Allison12345 <2562392373@qq.com>
2017-10-27 18:30:21 +0800

    class5.js

--- class4.js
+++ class4.js
@@ -1,9 +1,10 @@
-var x = 4,y = x++
-console.log(x,y)
+var x = 4,
+  y = x++
+console.log(x, y)
 var y = ++x
-console.log(x,y)
+console.log(x, y)
 var a = 4
 b = -a
-
-b =  +'3'
+console.log(b)
+b = +'3'
 console.log(b)
