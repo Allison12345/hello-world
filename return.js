b5169ec23c55f022f880236d816f7e486f4f4ee9
Allison12345 <2562392373@qq.com>
2017-11-04 21:30:07 +0800

    return.js

---
+++ return.js
@@ -0,0 +1,12 @@
+function add(x,y){
+    var result = x + y
+    return result
+}
+var result1 = add(1,2)
+console.log(result1)
+console.log(add(4,5))
+
+function createArry(x,y,z){
+    return [x,y,z]
+}
+console.log(createArry(5,6,7)[1])
