14906e9630fae9980d44fe386d61f32bd3403de1
Allison12345 <2562392373@qq.com>
2018-03-25 16:31:29 +0800

    evevt.js

---
+++ event.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+  var images = document.getElementsByTagName('img')
+  for (var i = 0; i < images.length; i++) {
+    var image = images[i]
+    if (image.addEventListener) {
+      image.addEventListener('click,hide,flase')
+    } else {
+      image.attachEvent('onclick', hide)
+    }
+  }
+  function hide(event) {
+    event.target.style.visibility = 'hidden'
+  }
+}
