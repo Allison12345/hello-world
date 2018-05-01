b0f5e0a35c05cc9157de4c02ab55ad13cbcaeffa
Allison12345 <2562392373@qq.com>
2018-05-01 14:12:20 +0800

    class.js

--- class.js
+++ class.js
@@ -1,13 +1,24 @@
-class Student {
+class People {
   constructor(abc) {
-    console.log('contructor',abc)
+    console.log('contructor', abc)
     this.name = abc
   }
-  getName(){
-      return this.name
+  getName() {
+    return this.name
   }
 }
-const stu1 = new Student('xiaoguai')
+class Student extends People {
+  constructor(name, score) {
+    super(name)
+    this.score = score
+  }
+  getName() {
+    return this.name + this.score
+  }
+}
+const stu1 = new Student('xiaoguai', 'cute')
 console.log(stu1.getName())
-const stu2 = new Student('hemu')
+const stu2 = new Student('hemu', 'ugly')
 console.log(stu2.getName())
+const peo = new Student('hehe', '23')
+console.log(peo.getName())
