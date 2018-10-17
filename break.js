5b8cca82179cc1e653c75046c123f0f488cfa2d7
Allison12345 <2562392373@qq.com>
2018-10-17 17:59:55 +0800

    throw.js

--- break.js
+++ break.js
@@ -1,17 +1,15 @@
-var matrxi = [1,2,3,4,5]
-var sum = 0,success = false
-compute_sum: if(matrxi){
-    for(var x= 0;x<matrxi.length;x++){
-        var cell = matrxi[x]
-        if(!cell) break compute_sum
-        for(y=0;y<cell.length;y++){
-            var row = cell[y]
-            if(isNaN(row)) break compute_sum
-            sum += row
-        }
-        success = true
+var matrxi = getData()
+var sum = 0,
+  success = false
+compute_sum: if (matrxi) {
+  for (var x = 0; x < matrxi.length; x++) {
+    var cell = matrxi[x]
+    if (!cell) break compute_sum
+    for (y = 0; y < cell.length; y++) {
+      var row = cell[y]
+      if (isNaN(row)) break compute_sum
+      sum += row
     }
-    return sum
+    success = true
+  }
 }
-
-console.log(matrxi)
--- throw.js
+++ throw.js
@@ -1,15 +1,14 @@
 function factorial(x) {
   if (x < 0) throw new Error('x不能是负数')
-  if (x > 1)  (x = x * factorial(x-1))
+  if (x > 1) x *= factorial(x - 1)
   return x
 }
 console.log(factorial(-2))
 
-try{
-    var n = Number(prompt("请输入一个正数"))
-    var f = factorial(n)
-    alert9(n = "!=" + f)
-}
-catch(e){
-    alert(e)
+try {
+  var n = Number(prompt('请输入一个正数'))
+  var f = factorial(n)
+  alert9((n = '!=' + f))
+} catch (e) {
+  alert(e)
 }
