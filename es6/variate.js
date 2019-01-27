ebf06aeece78891385803005dea6de5268b94db3
Allison12345 <2562392373@qq.com>
2019-01-27 21:53:14 +0800

    Deconstruction assignment

--- es6/variate.js
+++ es6/variate.js
@@ -15,6 +15,50 @@ let [h, f = '1'] = ['a', undefined]
 let { foe: baz } = { foe: 'aaa', bar: 'bbb' }
 let obj = { p: ['hello', { z: 'world' }] }
 let {
-  p,p: [c, { v }]
+  p,
+  p: [c, { v }]
 } = obj
-console.log(p)
+
+function example() {
+  return [1, 2, 3]
+}
+let [n, m, l] = example()
+function exampley() {
+  return { aijiao: 1, aijian: 2 }
+}
+let { aijiao, aijian } = exampley()
+
+let jsonDate = {
+  id: 42,
+  status: 'ok',
+  data: [24785398475]
+}
+let { id, status, data: number } = jsonDate
+
+//set default
+aiax = function(
+  url,
+  {
+    async = true,
+    beforeSend = function() {},
+    cache = true,
+    complete = function() {},
+    crossDomain = false,
+    global = true
+  }
+) {}
+
+const map = new Map()
+map.set('first', 'hello')
+map.set('second', 'world')
+for (let [key, value] of map) {
+  console.log(key + ' is ' + value)
+}
+//获取健名
+for (let [key] in map) {
+}
+//获取键值
+for (let [, value] in map) {
+}
+//const { SourceMapConsumer, SourceNode } = require('source-map')
+console.log(id, status, number)
