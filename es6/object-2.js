c387814853bb8140e7df56bafe0fdbc52248c4a9
Allison12345 <2562392373@qq.com>
2019-02-12 20:16:15 +0800

    object Incremental sales method

--- es6/object-2.js
+++ es6/object-2.js
@@ -84,41 +84,159 @@ const obj4 = {
 }
 console.log(Object.getOwnPropertyDescriptors(obj4))
 
-function getOwnPropertyDescriptors(obj){
-  const result  ={}
-  for(let key of Reflect.ownKeys(obj)){
+function getOwnPropertyDescriptors(obj) {
+  const result = {}
+  for (let key of Reflect.ownKeys(obj)) {
     result[key] = Object.getOwnPropertyDescriptor(obj)
   }
   return result
 }
 const source3 = {
   set foo(value) {
-    console.log(value);
+    console.log(value)
   }
-};
+}
 
-const target3= {};
-Object.assign(target1, source3);
+const target3 = {}
+Object.assign(target1, source3)
 console.log(Object.getOwnPropertyDescriptor(target3, 'foo'))
 
 const source5 = {
   set foo(value) {
-    console.log(value);
+    console.log(value)
   }
-};
+}
 
-const target5 = {};
-Object.defineProperties(target5, Object.getOwnPropertyDescriptors(source5));
+const target5 = {}
+Object.defineProperties(target5, Object.getOwnPropertyDescriptors(source5))
 console.log(Object.getOwnPropertyDescriptor(target5, 'foo'))
 
-const shallowMerge = (targe,source) => Object.defineProperties(
-  target,
-  Object.getOwnPropertyDescriptor(source)
-)
+const shallowMerge = (targe, source) =>
+  Object.defineProperties(target, Object.getOwnPropertyDescriptor(source))
 
-const clone = Object.create(Object.getPrototypeOf(obj),
-Object.getOwnPropertyDescriptors(obj))
-const shallowClone = (obj) => Object.create(
+const clone2 = Object.create(
   Object.getPrototypeOf(obj),
   Object.getOwnPropertyDescriptors(obj)
-);
+)
+const shallowClone = obj =>
+  Object.create(
+    Object.getPrototypeOf(obj),
+    Object.getOwnPropertyDescriptors(obj)
+  )
+
+Object.defineProperty(Object.prototype, '_proto_', {
+  get() {
+    let _thisObj = Object(this)
+    return Object.getPrototypeOf(_thisObj)
+  },
+  set(proto) {
+    if (this === undefined || this === null) {
+      throw new TypeError()
+    }
+    if (!isObject(this)) {
+      return undefined
+    }
+    if (isObject(proto)) {
+      return undefined
+    }
+    let status = Reflect.setPrototypeOf(this, proto)
+    if (!status) {
+      throw new TypeError()
+    }
+  }
+})
+function isObject(value) {
+  return Object(value) === value
+}
+
+let proto = {}
+let obj6 = { x: 10 }
+Object.setPrototypeOf(obj6, proto)
+
+function setPrototypeOf(obj, proto) {
+  obj._proto_ = proto
+  return obj
+}
+proto.y = 20
+proto.z = 40
+console.log(obj6.x)
+console.log(obj6.z)
+
+console.log(Object.setPrototypeOf(1, {}))
+console.log(Object.setPrototypeOf('true', {}))
+console.log(Object.setPrototypeOf('foo', {}))
+
+function Rectangle() {}
+
+const rec = new Rectangle()
+Object.getPrototypeOf(rec) === Rectangle.prototype //true
+Object.setPrototypeOf(rec, Object.prototype)
+Object.getPrototypeOf(rec) === Rectangle.prototype //false
+
+console.log(Object.getPrototypeOf(1))
+console.log(Object.getPrototypeOf('foo'))
+console.log(Object.getPrototypeOf(true))
+
+Object.getPrototypeOf(1) === Number.prototype
+Object.getPrototypeOf('foo ') === String.prototype
+Object.getPrototypeOf(true) === Boolean.prototype
+
+var obj7 = { foo: 'bar', baz: 42 }
+console.log(Object.keys(obj7))
+
+let { keys, values, entries } = Object
+let obj8 = { a: 1, b: 2, c: 3 }
+for (let key of keys(obj8)) {
+  console.log(key)
+}
+for (let value of values(obj8)) {
+  console.log(value)
+}
+for (let [key, value] of entries(obj8)) {
+  console.log([key, value])
+}
+
+var item = Math.floor(Math.random() * 1000)
+console.log(item)
+
+console.log(Object.values(obj8))
+const obj9 = { 100: 'a', 2: 'b', 7: 'c' }
+console.log(obj9)
+
+const obj10 = Object.create(
+  {},
+  {
+    p: {
+      value: 42,
+      enumerable: true
+    }
+  }
+)
+console.log(obj10)
+console.log(Object.values('foo'))
+
+console.log(Object.entries(obj8))
+let obj11 ={one:1,two:2}
+for(let [k,v ] of Object.entries(obj11)){
+  console.log(
+    `${JSON.stringify(k)} :${JSON.stringify(v)}`
+  )
+}
+
+const obj12 = {foo:'baz',bar :'42'}
+const map = new Map(Object.entries(obj12))
+console.log(map)
+
+function*entries1(obj){
+  for(let key of Object.keys(obj)){
+    yield [key,obj[key]]
+  }
+}
+function entries2(obj){
+  let arr = []
+  for (let key of Object.keys(obj)){
+    arr.push([key,obj[key]])
+  }
+  return arr
+}
+
